0355eeeea639a92113b51ef2b3715a1edeefff08
Madaline Albright <fenesa111@gmail.com>
2020-09-18 19:10:17 +0300

    adding files

---
+++ database/migrations/2016_08_12_230722_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_products', function (Blueprint $table) {
+            $table->increments('ID');
+            $table->string('pName')->nullable();
+            $table->integer('quantity');
+            $table->integer('price')->default(0);
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tbl_products');
+    }
+}
